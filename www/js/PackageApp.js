4f186146b3f3a92e81c496e431a430e9488d9acf
motorro <motorro@gmail.com>
2014-09-27 10:38:31 +0400

    Skeleton...

---
+++ www/js/PackageApp.js
@@ -0,0 +1,8 @@
+/**
+ * PackageApp
+ * This one is distributed with application and loads update package
+ * User: motorro
+ * Date: 27.09.2014
+ * Time: 9:51
+ */
+var updateURL = "#{updateURL}";
--- www/js/PreloaderApp.js
+++ www/js/PreloaderApp.js
@@ -1,5 +1,6 @@
 /**
- * App.js
+ * PreloaderApp
+ * This one is distributed with application and evaluates the latest version
  * User: motorro
  * Date: 27.09.2014
  * Time: 9:28
