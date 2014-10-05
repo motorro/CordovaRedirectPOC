bf4d7679ffef298ab187082eb6841519b41237ea
motorro <motorro@gmail.com>
2014-10-05 14:59:36 +0400

    Packaged app returns to preloader

--- app/PackageApp.js
+++ app/PackageApp.js
@@ -21,14 +21,45 @@ readyTrigger(function(){
 
     function runCommand(command) {
         startButton.setEnabled(false);
-        resetButton.setEnabled(false);
+        preloaderButton.setEnabled(false);
         command().fin(function(){
             startButton.setEnabled(true);
-            resetButton.setEnabled(true);
+            preloaderButton.setEnabled(true);
         });
     }
 
     var startButton = new Button("start", function() {
         runCommand(startWorkflow);
     });
+    var preloaderButton = new Button("preloader", function() {
+        runCommand(toPreloader);
+    });
 });
+
+/**
+ * Starts POC workflow
+ * 1) Check storage folder for update
+ * 2) If update found - redirect there
+ * 3) If not - redirect to package.html
+ */
+function startWorkflow() {
+    var d = Q.defer();
+    d.resolve();
+    return d.promise;
+}
+
+/**
+ * Returns to preloader
+ */
+function toPreloader() {
+    var d = Q.defer();
+    log ("Going back to preloader...");
+    window.setTimeout(
+        function() {
+            window.location.href = "preloader.html";
+        },
+        500
+    );
+    d.resolve();
+    return d.promise;
+}
