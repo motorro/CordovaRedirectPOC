37be1bbab066a2ba98d59d10fb2ad48a76a6db9c
motorro <motorro@gmail.com>
2014-10-22 06:09:09 +0400

    Assets and assets override build

--- hooks/before_prepare/020_build_assets.js
+++ hooks/before_prepare/020_build_assets.js
@@ -36,52 +36,54 @@ if (false === fs.existsSync(SOURCE_DIR)) {
     throw new Error("Source directory not found!");
 }
 
-
-var b = browserify();
-b.require(hooksUtils.streams.createStringStream("module.exports=[1,2,3]"));
-b.bundle().pipe(process.stdout);
-// TODO: Отэта нужно использовать для экспорта оверрайдов
-// Сформировать объект
-require('./vendor/angular/angular.js', {expose: 'assetOverrides'});
+var staticSource = path.join(SOURCE_DIR, "static");
+var staticDestination = path.join(DESTINATION_DIR, "static");
+var dynamicSource = path.join(SOURCE_DIR, "dynamic");
+var dynamicDestination = path.join(DESTINATION_DIR, "dynamic");
+var assetOverridesPath = path.join(DESTINATION_DIR, "assetOverrides.js");
 
 /*
  1. Cleanup destination dir
- 2. Copy all HTML files
- 3. Browserify all JS files in source dir
- 4. If building a release build - do the same with pre-loader
+ 2. Copy all static assets. In case of release it is a whole assets directory
+ 3. If building update - build a separate dynamic asset list - files loaded with JS that could be resolved on runtime.
+ 4. Form an assetOverrides module to resolve dynamic assets
  */
-Q.nfcall(rimraf, DESTINATION_DIR)
-    .then(function(){return Q.nfcall(hooksUtils.ensureDirExists, DESTINATION_DIR);})
-    .then(function(){return buildFileList(SOURCE_DIR)})
-    .then(function(list){
-        return Q.all([
-            (function() {
-                return hooksPromiseUtils.copyFiles(SOURCE_DIR, DESTINATION_DIR, list)
-                    .then(function(numCopied){
-                        console.log(numCopied + " assets copied.")
-                    })
-            })()
-        ]);
+var sequence =
+    Q.nfcall(rimraf, DESTINATION_DIR)
+    .then(function(){ return Q.nfcall(hooksUtils.ensureDirExists, DESTINATION_DIR); })
+    // Copy static assets
+    .then(function(){ Q.nfcall(hooksUtils.ensureDirExists, staticDestination); })
+    .then(function(){ return buildReleaseFileList(staticSource); })
+    .then(function(list){ return hooksPromiseUtils.copyFiles(staticSource, staticDestination, list); })
+    .then(function(numCopied){
+        console.log(numCopied + " static assets copied.")
     })
-    .done();
+    // Copy dynamic files
+    .then(function(){ Q.nfcall(hooksUtils.ensureDirExists, dynamicDestination); });
 
-function buildFileList(srcDir) {
     if ("update" === process.env.BUILD_TYPE) {
-        return Q.all([
-            buildReleaseFileList(path.join(SOURCE_DIR, "static")),
-            buildUpdateFileList(path.join(SOURCE_DIR, "dynamic"))
-        ]).then(function(lists){
-            return lists.reduce(
-                function(soFar, list){
-                    return soFar.concat(list);
-                },
-                []
-            );
+        sequence = sequence.then(function() {
+            return buildUpdateFileList(dynamicSource)
+        })
+        .then(function(list) {
+            // Build a list of overrides
+            return buildAssetOverrideList(list, assetOverridesPath).thenResolve(list);
         });
     } else {
-        return buildReleaseFileList(SOURCE_DIR);
+        sequence = sequence.then(function() {
+            return buildReleaseFileList(dynamicSource)
+        })
+        .then(function(list) {
+            // Build an empty list to speedup resolve
+            return buildAssetOverrideList([], assetOverridesPath).thenResolve(list);
+        });
     }
-}
+
+    sequence = sequence.then(function(list){ return hooksPromiseUtils.copyFiles(dynamicSource, dynamicDestination, list); })
+    .then(function(numCopied){
+        console.log(numCopied + " dynamic assets copied.")
+    })
+    .done();
 
 /**
  * If in 'release' mode - copy all assets
@@ -96,4 +98,41 @@ function buildReleaseFileList(srcDir) {
  */
 function buildUpdateFileList(srcDir) {
     return Q.nfcall(glob, "**/*.*", {cwd:srcDir});
+}
+
+/**
+ * Creates a browserify module to resolve resources
+ * @param files File list
+ * @param module Output module file
+ */
+function buildAssetOverrideList(files, module) {
+    var result = Q.defer();
+
+    // Make an object from file array to speed-up asset resolving
+    var fileMap = {};
+    var i = files.length;
+    while (--i >= 0) {
+        fileMap[files[i]] = true;
+    }
+
+    var output = fs.createWriteStream(module);
+    output.on("error", function (err) {
+        result.reject(err);
+    });
+    output.on('close', function () {
+        result.resolve();
+    });
+
+    var b = browserify();
+    b.require(
+        hooksUtils.streams.createStringStream("module.exports=" + JSON.stringify(fileMap)),
+        {expose: 'assetOverrides'}
+    );
+    var bundle = b.bundle();
+    bundle.pipe(output);
+    bundle.on("error", function (err) {
+        result.reject(err);
+    });
+
+    return result.promise;
 }
